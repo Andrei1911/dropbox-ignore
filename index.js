08c7423db5b9f7f21b50ec077c01b54db2deb321
Andrei Popa <andrei@audaciousleap.com>
2023-05-13 14:08:46 +0300

    working version

--- index.js
+++ index.js
@@ -4,50 +4,96 @@ const fs = require('fs');
 const { exec } = require('child_process');
 const path = require('path');
 
-function setDropboxIgnore(directory, file) {
-  const filePath = path.join(directory, file);
-  exec(`Set-Content -Path '${filePath}' -Stream com.dropbox.ignored -Value 1`, (err) => {
+function setDropboxIgnore(file) {
+  fs.access(file, fs.constants.F_OK, (err) => {
     if (err) {
-      console.error('Error setting Dropbox to ignore file:', err);
-    } else {
-      console.log(`Dropbox has been set to ignore ${file}`);
+      console.log(`File or directory ${file} does not exist. Skipping.`);
+      return;
     }
-  });
-}
-
-function unsetDropboxIgnore(directory, file) {
-  const filePath = path.join(directory, file);
-  exec(`Clear-Content -Path '${filePath}' -Stream com.dropbox.ignored`, (err) => {
-    if (err) {
-      console.error('Error setting Dropbox to unignore file:', err);
-    } else {
-      console.log(`Dropbox has been set to unignore ${file}`);
-    }
-  });
-}
-
-function processDropboxIgnoreFile(directory, unignore) {
-  const dropboxIgnoreFilePath = path.join(directory, '.dropboxignore');
-
-  fs.access(dropboxIgnoreFilePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      // .dropboxignore file does not exist, so create it
-      fs.writeFileSync(dropboxIgnoreFilePath, 'node_modules\n');
-    }
-
-    const files = fs.readFileSync(dropboxIgnoreFilePath, 'utf-8').split('\n');
-    files.forEach(file => {
-      if (file) {
-        if (unignore) {
-          unsetDropboxIgnore(directory, file);
-        } else {
-          setDropboxIgnore(directory, file);
-        }
+    exec(`Set-Content -Path '${file}' -Stream com.dropbox.ignored -Value 1`, { shell: 'powershell.exe' }, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error setting Dropbox to ignore ${file}:`, err);
+      } else {
+        console.log(`Dropbox has been set to ignore ${file}`);
       }
     });
   });
 }
 
-const currentDirectory = process.cwd();
-const unignore = process.argv.includes('-u');
-processDropboxIgnoreFile(currentDirectory, unignore);
+function clearDropboxIgnore(file) {
+  fs.access(file, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log(`File or directory ${file} does not exist. Skipping.`);
+      return;
+    }
+    exec(`Clear-Content -Path '${file}' -Stream com.dropbox.ignored`, { shell: 'powershell.exe' }, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error clearing Dropbox ignore for ${file}:`, err);
+      } else {
+        console.log(`Dropbox ignore has been cleared for ${file}`);
+      }
+    });
+  });
+}
+
+function processDropboxIgnoreFile(directory, dropboxIgnoreFilePath, clear = false) {
+  fs.readFile(dropboxIgnoreFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading .dropboxignore file:', err);
+      return;
+    }
+    const lines = data.split('\n');
+    for (let line of lines) {
+      line = line.trim();
+      if (line) {
+        if (clear) {
+          clearDropboxIgnore(path.join(directory, line));
+        } else {
+          setDropboxIgnore(path.join(directory, line));
+        }
+      }
+    }
+  });
+}
+
+function createDropboxIgnoreFile(directory, clear = false) {
+  try {
+    const dropboxIgnoreFilePath = path.join(directory, '.dropboxignore');
+
+    fs.access(dropboxIgnoreFilePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        // .dropboxignore file does not exist, so create it
+        let content = '';
+        const nodeModulesPath = path.join(directory, 'node_modules');
+
+        fs.access(nodeModulesPath, fs.constants.F_OK, (err) => {
+          if (!err) {
+            // node_modules directory exists
+            content = 'node_modules\n';
+          }
+
+          fs.writeFile(dropboxIgnoreFilePath, content, (err) => {
+            if (err) {
+              console.error('Error writing .dropboxignore file:', err);
+            } else {
+              console.log('.dropboxignore file has been created');
+              processDropboxIgnoreFile(directory, dropboxIgnoreFilePath, clear);
+            }
+          });
+        });
+      } else {
+        console.log('.dropboxignore file already exists');
+        processDropboxIgnoreFile(directory, dropboxIgnoreFilePath, clear);
+      }
+    });
+  } catch (error) {
+    console.error("Error in dropbox-ignore script:\n", error)
+  }
+}
+
+// Call the function directly, passing in the current directory
+if (process.argv[2] === 'clear') {
+  createDropboxIgnoreFile(process.cwd(), true);
+} else {
+  createDropboxIgnoreFile(process.cwd());
+}
